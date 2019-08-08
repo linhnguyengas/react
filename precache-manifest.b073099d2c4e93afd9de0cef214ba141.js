3f42c369285a08e5e94277eed84f231a6a14828b
linhnguyengas <linhgaster@gmail.com>
2019-08-08 20:54:59 +0700

    Updates

---
+++ precache-manifest.b073099d2c4e93afd9de0cef214ba141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34ae45e56a6fe92bae876beb823f3e1",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6acdc035d9cedba82ff1",
+    "url": "/react/static/css/main.2a34d720.chunk.css"
+  },
+  {
+    "revision": "55561675ec49779bcb9e",
+    "url": "/react/static/js/2.58717ca0.chunk.js"
+  },
+  {
+    "revision": "6acdc035d9cedba82ff1",
+    "url": "/react/static/js/main.fc6ef4df.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "4fc3d361dd4994ce0550bae1b9a16a56",
+    "url": "/react/static/media/avatar.4fc3d361.png"
+  }
+]);
